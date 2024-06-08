f05e239e887701fcc41f17ac716bf3ee9015615c
Dudi Eden <edoodi1@gmail.com>
2024-06-08 19:25:00 +0300

    store guild details

--- src/events/clientReadyEventHandler.js
+++ src/events/clientReadyEventHandler.js
@@ -1,17 +1,23 @@
 const { Events } = require("discord.js");
 
 var activeGuilds = [];
+var activeGuildsIds = [];
 
 module.exports = {
   name: Events.ClientReady,
   once: true,
   execute(client) {
     console.log(`Ready! Logged in as ${client.user.tag}`);
-
-    let guildCache = client.guilds.cache;
-    for (const [key, value] of guildCache) {
-      activeGuilds.push(value);
-    }
-    console.log(`Guilds List: ${activeGuilds}`);
+    client.guilds.cache.forEach((element) => {
+      let tempGuildName = element.name;
+      let tempGuildId = element.id;
+      if (!activeGuilds.includes(tempGuildName)) {
+        console.log(`guild not in list, adding... ${tempGuildName}`);
+        activeGuilds.push(tempGuildName);
+        activeGuildsIds.push(tempGuildId);
+      }
+    });
+    console.log(`*******  Guilds List [Names]: ${activeGuilds}`);
+    console.log(`*******  Guilds List [Ids]: ${activeGuilds}`);
   },
 };
