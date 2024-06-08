8f5eb688b51f974c5944ab1610fb7cd60f4a5d11
Dudi Eden <edoodi1@gmail.com>
2024-06-08 18:43:15 +0300

    save all active guilds in a list

--- src/events/clientReadyEventHandler.js
+++ src/events/clientReadyEventHandler.js
@@ -1,9 +1,17 @@
 const { Events } = require("discord.js");
 
+var activeGuilds = [];
+
 module.exports = {
   name: Events.ClientReady,
   once: true,
   execute(client) {
     console.log(`Ready! Logged in as ${client.user.tag}`);
+
+    let guildCache = client.guilds.cache;
+    for (const [key, value] of guildCache) {
+      activeGuilds.push(value);
+    }
+    console.log(`Guilds List: ${activeGuilds}`);
   },
 };
