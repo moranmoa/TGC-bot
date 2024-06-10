662f4441bc4279e097a8166ea9a770abb90846be
Moran <moranmoa@gmail.com>
2024-06-10 23:14:38 +0300

    remove stck channels

--- src/events/clientReadyEventHandler.js
+++ src/events/clientReadyEventHandler.js
@@ -1,4 +1,5 @@
 const { Events } = require("discord.js");
+const {getGuildData ,setGuildData} = require('./activityUtils');
 
 var activeGuilds = [];
 var activeGuildsIds = [];
@@ -9,6 +10,7 @@ module.exports = {
   execute(client) {
     console.log(`Ready! Logged in as ${client.user.tag}`);
     client.guilds.cache.forEach((element) => {
+      resetEmptyChannels(element)
       let tempGuildName = element.name;
       let tempGuildId = element.id;
       if (!activeGuilds.includes(tempGuildName)) {
@@ -21,3 +23,32 @@ module.exports = {
     console.log(`*******  Guilds List [Ids]: ${activeGuilds}`);
   },
 };
+
+//remove stuch chanels
+async function resetEmptyChannels(guild) {
+  var guildData = await getGuildData(guild.id);
+
+  // Filter out and delete empty channels
+  guildData.aActiveChannels = guildData.aActiveChannels.filter((activeChannel) => {
+    try {
+      const channel = guild.channels.cache.get(activeChannel.id);
+      if (channel.members.size == 0) {
+        guild.channels
+          .delete(activeChannel.id, "making room for new channels")
+          // .then(console.log)
+          .catch(console.error);
+        return false; // Remove from the array
+      }
+    } catch (e) {
+      guild.channels
+        .delete(activeChannel.id, "making room for new channels")
+        // .then(console.log)
+        .catch(console.error);
+      return false; // Remove from the array
+    }
+    return true; // Keep in the array
+  });
+
+  // Save the updated guildData if needed
+  await setGuildData(guild.id, guildData);
+}
