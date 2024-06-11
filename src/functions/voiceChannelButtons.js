a11a59093e5ef29a9ab898e97a2fc1440e991875
Dudi Eden <edoodi1@gmail.com>
2024-06-11 23:17:03 +0300

    disable all button related commits

--- src/functions/voiceChannelButtons.js
+++ src/functions/voiceChannelButtons.js
@@ -1,53 +1,53 @@
-const {
-  ActionRowBuilder,
-  ButtonBuilder,
-  ButtonStyle,
-  ComponentType,
-} = require("discord.js");
+// const {
+//   ActionRowBuilder,
+//   ButtonBuilder,
+//   ButtonStyle,
+//   ComponentType,
+// } = require("discord.js");
 
-const { getGuildData } = require("../events/activityUtils");
+// const { getGuildData } = require("../events/activityUtils");
 
-async function getOwner(data, channel) {
-  let index = data.aActiveChannels.findIndex((data) => data.id === channel.id);
-  return data.aActiveChannels[index]["master"];
-}
+// async function getOwner(data, channel) {
+//   let index = data.aActiveChannels.findIndex((data) => data.id === channel.id);
+//   return data.aActiveChannels[index]["master"];
+// }
 
-async function sendButtons(channel) {
-  const firstButton = new ButtonBuilder()
-    .setLabel("Edit Name")
-    .setStyle(ButtonStyle.Primary)
-    .setCustomId("edit-name");
+// async function sendButtons(channel) {
+//   const firstButton = new ButtonBuilder()
+//     .setLabel("Edit Name")
+//     .setStyle(ButtonStyle.Primary)
+//     .setCustomId("edit-name");
 
-  const secondButton = new ButtonBuilder()
-    .setLabel("second button")
-    .setStyle(ButtonStyle.Primary)
-    .setCustomId("second-button");
+//   const secondButton = new ButtonBuilder()
+//     .setLabel("second button")
+//     .setStyle(ButtonStyle.Primary)
+//     .setCustomId("second-button");
 
-  const buttonRow = new ActionRowBuilder().addComponents(firstButton);
+//   const buttonRow = new ActionRowBuilder().addComponents(firstButton);
 
-  const reply = await channel.send({
-    components: [buttonRow],
-  });
+//   const reply = await channel.send({
+//     components: [buttonRow],
+//   });
 
-  // Get the Channel Owner
-  var guildData = await getGuildData(channel.guildId);
-  var ownedBy = getOwner(guildData, channel);
-  const filter = (i) => i.user.id === ownedBy;
+//   // Get the Channel Owner
+//   var guildData = await getGuildData(channel.guildId);
+//   var ownedBy = getOwner(guildData, channel);
+//   const filter = (i) => i.user.id === ownedBy;
 
-  const collector = reply.createMessageComponentCollector({
-    ComponentType: ComponentType.Button,
-    filter, // filter this collector to be accessible by the channel owner only
-    time: 10 * 1000, // sets a time limit to the interaction 10ms * 1000 = 10seconds
-  });
+//   const collector = reply.createMessageComponentCollector({
+//     ComponentType: ComponentType.Button,
+//     filter, // filter this collector to be accessible by the channel owner only
+//     // time: 10 * 1000, // sets a time limit to the interaction 10ms * 1000 = 10seconds
+//   });
 
-  // Collector time edit the reply and disables these buttons
-  collector.on("end", () => {
-    firstButton.setDisabled(true);
-    secondButton.setDisabled(true);
-    reply.edit({
-      components: [buttonRow],
-    });
-  });
-}
+//   // Collector time edit the reply and disables these buttons
+//   // collector.on("end", () => {
+//   //   firstButton.setDisabled(true);
+//   //   secondButton.setDisabled(true);
+//   //   reply.edit({
+//   //     components: [buttonRow],
+//   //   });
+//   // });
+// }
 
-module.exports = { sendButtons };
+// module.exports = { sendButtons };
