be9272b9bcf72e04f85f6ea5f7ff6e83392fdbb9
Dudi Eden <edoodi1@gmail.com>
2024-06-12 02:14:28 +0300

    convert new channel creation file name and add call to the channel logger

---
+++ src/events/newChannelEventHandler.js
@@ -0,0 +1,29 @@
+const { Events } = require("discord.js");
+const { sendButtons } = require("../functions/voiceChannelButtons");
+const { getGuildData } = require("./activityUtils");
+const log4js = require("log4js");
+const {} = require("../logger");
+const appLogger = log4js.getLogger("channel");
+
+// // This sends the buttons to a newly automated created voice channel
+
+module.exports = {
+  name: Events.ChannelCreate,
+  async execute(channel) {
+    console.log(channel);
+    appLogger.log("New Channel Created", {
+      iD: channel.id,
+      name: channel.name,
+    });
+
+    // await new Promise((resolve) => setTimeout(resolve, 1000)); // wait 1 second
+    // we must wait few seconds before reading the file, if not we might read a NULL one.
+    // var guildData = await getGuildData(channel.guildId);
+    // let index = guildData.aActiveChannels.findIndex(
+    //   (data) => data.id === channel.id
+    // );
+    // if (index !== -1) {
+    //   sendButtons(channel);
+    // }
+  },
+};
