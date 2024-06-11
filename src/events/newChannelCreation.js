a1961879aa673bd759e0280247ca4dc36d09f75b
Dudi Eden <edoodi1@gmail.com>
2024-06-11 22:15:11 +0300

    shorten the wait time

--- src/events/newChannelCreation.js
+++ src/events/newChannelCreation.js
@@ -7,16 +7,15 @@ const { getGuildData } = require("../events/activityUtils");
 module.exports = {
   name: Events.ChannelCreate,
   async execute(channel) {
-    await new Promise((resolve) => setTimeout(resolve, 5000));
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     // we must wait few seconds before reading the file, if not we might read a NULL one.
 
     var guildData = await getGuildData(channel.guildId);
-    console.log(`Guild DATA = ${guildData}`);
+
     let index = guildData.aActiveChannels.findIndex(
       (data) => data.id === channel.id
     );
 
-    console.log(`the index is ${index}`);
     if (index !== -1) {
       console.log(`New Channel Created: ${channel.id}`);
       sendButtons(channel);
